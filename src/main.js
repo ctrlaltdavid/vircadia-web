135baa7412d838b920fee15f4ed5bf36ce46a202
David Rowe <david@ctrlaltstudio.com>
2021-05-10 12:48:47 +1200

    SDK stub

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Vircadia from "../vircadia-web-sdk/dist/Vircadia.js";
+
+
 createApp(App).mount('#app')
+
+Vircadia.helloWorld();
